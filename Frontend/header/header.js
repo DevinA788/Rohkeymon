b1dd7dcfe0c3ca509a567878470e8425a0a08510
DevinA788 <devin.ray27@gmail.com>
2025-12-01 13:22:26 -0600

    Shifted right-side divs to right side of screen.

--- Frontend/header/header.js
+++ Frontend/header/header.js
@@ -4,23 +4,23 @@ window.addEventListener('load', function() {
   header.innerHTML = `    
   <div class="left-section">
       <div class="logo-container">
-      <a href="/set-lists/index.html"><img class="site-logo" src="/header/TempLogo.png"></a>
+        <a href="/set-lists/index.html"><img class="site-logo" src="/header/TempLogo.png"></a>
       </div>
+  </div>
+  <div class="middle-section">
+    <div class="sets-link">
+      <a href="/set-lists/index.html">Sets</a>
     </div>
-    <div class="middle-section">
-      <div class="sets-link">
-        <a href="/set-lists/index.html">Sets</a>
-      </div>
-      <div class="decks-link">
-        <a href="">Decks</a>
-      </div>
+    <div class="decks-link">
+      <a href="">Decks</a>
     </div>
-    <div class="right-section">
-      <div class="sign-in-link">
-        <a href="">Sign in</a>
-      </div>
-      <div class="register-link">
-        <a href="">Register</a>
-      </div>
-    </div>`;
+  </div>
+  <div class="right-section">
+    <div class="sign-in-link">
+      <a href="">Sign in</a>
+    </div>
+    <div class="register-link">
+       <a href="">Register</a>
+    </div>
+  </div>`;
 });
