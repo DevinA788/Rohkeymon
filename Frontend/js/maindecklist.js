b4f6057a8947c70346ba0b6e73b4787d858c2943
DevinA788 <devin.ray27@gmail.com>
2025-12-02 12:12:25 -0600

    Worked on div creation loops

---
+++ Frontend/js/maindecklist.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', function() {
+  //DOM - learn it 
+  const setList=document.getElementById("pokemonlist")
+  fetch("http://127.0.0.1:8080/Rohkeymon/alldata").then(r=>r.json()).then(d=>{
+    d.forEach(
+      p=>{
+        const card=document.createElement("div")
+        const img=document.createElement("img")
+        img.src=`https://images.pokemontcg.io/base1/${p.card_id}.png`
+        card.className="card-item"
+        card.appendChild(img)
+        setList.appendChild(card)
+      }
+    )
+    console.log(d); /*Subject to CSS if user input is introduced*/
+    
+    // NOTE: innerText for variables.
+  })
+});
+
+//<div class="card-item">
+//<img src="https://images.pokemontcg.io/xy1/1.png" width="100" />
+//</div>
+
+//https://images.pokemontcg.io/base1/1.png
+
+//input box where typing names calls 3rd party api to bring up cards with + button to add card to user's deck
---
+++ Frontend/templates/header.js
@@ -0,0 +1,9 @@
+document.querySelector('#header');
+
+window.addEventListener('load', function() {
+  fetch("/templates/header.template.html?v=1").then(r=>r.text()).then(d=>{
+    console.log(d); /*Subject to CSS if user input is introduced*/
+    document.getElementById("header").innerHTML=d;
+    // NOTE: innerText for variables.
+  })
+});
