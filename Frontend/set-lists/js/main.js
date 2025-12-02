d88902ea4786e3fa91adfacee6eb6bbf6d392f69
DevinA788 <devin.ray27@gmail.com>
2025-12-02 12:59:50 -0600

    Created loop to generate base set 1 divs.

--- Frontend/set-lists/js/main.js
+++ Frontend/set-lists/js/main.js
@@ -1,21 +1,19 @@
 window.addEventListener('load', function() {
   //DOM - learn it 
-  const setList=document.getElementById("pokemonlist")
-  fetch("http://127.0.0.1:8080/Rohkeymon/alldata").then(r=>r.json()).then(setlist=>{
-    setlist.forEach(
-      p=>{
+  const setList=document.getElementById("pokemondiv")
+    for (let i = 1; i < 103; i++){
         const card=document.createElement("div")
         const img=document.createElement("img")
-        img.src=`https://images.pokemontcg.io/base1/${p.card_id}.png`
+        img.src=`https://images.pokemontcg.io/base1/${i}.png`
         card.className="card-item"
         card.appendChild(img)
         setList.appendChild(card)
       }
-    )
-    console.log(d); /*Subject to CSS if user input is introduced*/
+    
+    console.log(i); /*Subject to CSS if user input is introduced*/
     
     // NOTE: innerText for variables.
-  })
+
 });
 
 //<div class="card-item">
