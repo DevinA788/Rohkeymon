1fa34f9966498a70e0a3ed2ee20af5ec75179a4d
DevinA788 <devin.ray27@gmail.com>
2025-12-17 12:21:50 -0600

    Started working on GET request for decklist tab to show deck.

--- Frontend/templates/decklistTab.js
+++ Frontend/templates/decklistTab.js
@@ -1,11 +1,13 @@
 document.querySelector('#decklistContainer');
 
+const deckUUID = "44764e09-bf3d-11f0-a784-d8bbc1d9bfc1" //Temporarily hardcoded until users can be set-up. 
 
 fetch("/templates/decklistTab.template.html?v=1").then(r=>r.text()).then(d=>{
   //console.log(d); /*Subject to CSS if user input is introduced*/
   document.getElementById("decklistContainer").innerHTML=d;
   // NOTE: innerText for variables.
   initializeDecklistTab();
+  showDecklist();
 });
 
 
@@ -24,3 +26,14 @@ function initializeDecklistTab() {
   });
 };
 
+function showDecklist(deckUUID) {
+  const response = await fetch("http://localhost:8080/api/alldata", {
+    method: "GET",
+    headers: {"Content-Type": "application/json",},
+    body: JSON.stringify({
+      card_copies: "1",
+      card_id: `base1-${id}`,
+      decklist_id: "44764e09-bf3d-11f0-a784-d8bbc1d9bfc1"
+    }),
+  })
+}
