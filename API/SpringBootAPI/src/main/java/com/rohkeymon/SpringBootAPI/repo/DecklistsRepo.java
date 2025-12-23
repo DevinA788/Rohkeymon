22e58d4b28535d8296bd1fd340840e2cfae41c76
DevinA788 <devin.ray27@gmail.com>
2025-12-23 13:55:51 -0600

    Changed API and frontend logic to use decklist card objects.

--- API/SpringBootAPI/src/main/java/com/rohkeymon/SpringBootAPI/repo/DecklistsRepo.java
+++ API/SpringBootAPI/src/main/java/com/rohkeymon/SpringBootAPI/repo/DecklistsRepo.java
@@ -1,6 +1,6 @@
 package com.rohkeymon.SpringBootAPI.repo;
 
-import com.rohkeymon.SpringBootAPI.model.Cards;
+import com.rohkeymon.SpringBootAPI.model.Decklists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -25,28 +25,28 @@ public class CardsRepo {
 
     private JdbcTemplate jdbcTemplate;
 
-    public void save(Cards cards){
+    public void save(Decklists decklists){
 
         String sqlInsert = "INSERT INTO cards (decklist_id, card_id, card_copies) VALUES (?, ?, ?);";
 
         
 
-        jdbcTemplate.update(sqlInsert, cards.getDecklist_id(), cards.getCard_id(), cards.getCard_copies());
+        jdbcTemplate.update(sqlInsert, decklists.getDecklist_id(), decklists.getCard_id(), decklists.getCard_copies());
 
     }
 
-    public List<Cards> findAll(){
-        RowMapper<Cards> rm = new BeanPropertyRowMapper<>(Cards.class);
+    public List<Decklists> findAll(){
+        RowMapper<Decklists> rm = new BeanPropertyRowMapper<>(Decklists.class);
         String sql = "SELECT * from cards;";
-        List<Cards> alldata;
+        List<Decklists> alldata;
         alldata = this.jdbcTemplate.query(sql, rm);
         return alldata;
     }
 
-    public List<Cards> decklistQuery(){
-        RowMapper<Cards> rm = new BeanPropertyRowMapper<>(Cards.class);
+    public List<Decklists> decklistQuery(){
+        RowMapper<Decklists> rm = new BeanPropertyRowMapper<>(Decklists.class);
         String sql = "SELECT * FROM cards WHERE decklist_id = '44764e09-bf3d-11f0-a784-d8bbc1d9bfc1';";
-        List<Cards> decklistQueryResult;
+        List<Decklists> decklistQueryResult;
         decklistQueryResult = this.jdbcTemplate.query(sql, rm);
         return decklistQueryResult;
     }
