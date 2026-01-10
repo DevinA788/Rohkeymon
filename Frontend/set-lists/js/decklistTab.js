8e712980f78f839dd0cbfcf89f9187118853b903
DevinA788 <devin.ray27@gmail.com>
2026-01-10 10:01:45 -0600

    Started building out decklist dropdown

--- Frontend/set-lists/js/decklistTab.js
+++ Frontend/set-lists/js/decklistTab.js
@@ -1,5 +1,20 @@
 import {cardId, decklist, count, decklistId, addingToDeck, loadDecklist} from "./main.js";
 
+document.addEventListener('click', e => {
+  const isDropdownButton = e.target.matches("[data-dropdown-button]")
+  if(!isDropdownButton && e.target.closest('[data-dropdown]') !=null) return
+
+  let currentDropdown
+  if(isDropdownButton) {
+    currentDropdown = e.target.closest('[data-dropdown]')
+    currentDropdown.classList.toggle('active')
+  }
+
+  document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+    if (dropdown === currentDropdown) return
+    dropdown.classList.remove("active")
+  })
+})
 
 export async function getName(card_id) {
   try {
