5892fbc5c5fc5a7efed8ed7435b915cfe9978ea1
DevinA788 <devin.ray27@gmail.com>
2026-01-29 19:28:23 -0600

    Working on SecurityFilterChain for managing authorization rules

--- API/SpringBootAPI/src/main/java/com/rohkeymon/SpringBootAPI/Security/SecurityConfig.java
+++ API/SpringBootAPI/src/main/java/com/rohkeymon/SpringBootAPI/Security/SecurityConfig.java
@@ -4,8 +4,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {}
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .formLogin(httpForm -> {
+                    httpForm
+                            .loginPage(loginPage:"/index.html").permitAll();
+                })
+    }
+}
