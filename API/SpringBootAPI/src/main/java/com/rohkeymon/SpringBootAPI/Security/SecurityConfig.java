bdaf38ad1abe9f13ea8dd3d2cab10611c9af0aa5
DevinA788 <devin.ray27@gmail.com>
2026-01-29 19:14:22 -0600

    @EnableWebSecurity to annotate config class so Spring Security can use its filter chain and create auth manager builder and auth builder

---
+++ API/SpringBootAPI/src/main/java/com/rohkeymon/SpringBootAPI/Security/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.rohkeymon.SpringBootAPI.Security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {}
