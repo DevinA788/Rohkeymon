385b368d45a65a29442f1620d4aa10862262c90a
DevinA788 <devin.ray27@gmail.com>
2026-02-11 19:27:32 -0600

    Set sessions to be stateless so that crsf protection is fine to disable.

--- API/SpringBootAPI/src/main/java/com/rohkeymon/SpringBootAPI/Security/SecurityConfig.java
+++ API/SpringBootAPI/src/main/java/com/rohkeymon/SpringBootAPI/Security/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.rohkeymon.SpringBootAPI.Security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -14,10 +16,9 @@ public class SecurityConfig {
 
     @Bean //Custom filter chain bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        http.csrf(customizer -> customizer.disable());
+        http.sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
-                /*.csrf(AbstractHttpConfigurer::disable)
-                .formLogin(httpForm ->{
-                    httpForm.loginPage("/req/login").permitAll();
-                    httpForm.defaultSuccessUrl("/index"); */
     }
 }
