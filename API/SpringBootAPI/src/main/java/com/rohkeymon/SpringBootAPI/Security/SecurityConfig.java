92d4d2ec34aa9be45d6499069426524297e44915
DevinA788 <devin.ray27@gmail.com>
2026-02-27 08:22:22 -0600

    Imported bcyrpt for password encoding method.

--- API/SpringBootAPI/src/main/java/com/rohkeymon/SpringBootAPI/Security/SecurityConfig.java
+++ API/SpringBootAPI/src/main/java/com/rohkeymon/SpringBootAPI/Security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -25,5 +26,10 @@ public class SecurityConfig {
                 .anyRequest().authenticated()
         );
         return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
